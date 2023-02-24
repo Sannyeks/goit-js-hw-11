cbe9c53f512377a28f1df6a5cf33bea20e1aad2a
Oleksandr Yushchenko <sanyeks@gmail.com>
2023-02-24 17:59:02 +0200

    add

--- src/index.js
+++ src/index.js
@@ -125,12 +125,14 @@ function clearAll() {
     refs.allPhotos.innerHTML = '';
 }
 
-function smoothScroll() {
-    const { height: cardHeight } = refs.gallery
-    .firstElementChild.getBoundingClientRect();
-    window.scrollBy({
-        top: cardHeight * 2,
-        behavior: "smooth",
-    });
+function smoothScroll(){
+  const { height: cardHeight } = document
+  .querySelector(".gallery")
+  .firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+  top: cardHeight * 2,
+  behavior: "smooth",
+});
 }
 // 
